ed25ceb66b718a822168446677538f61e3f6fafe
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-01 17:24:36 +0300

    Moved the refresh logic to interceptors

--- src/service/initialize.ts
+++ src/service/initialize.ts
@@ -1,45 +1,50 @@
-import authorizationAPI from '../store/authorization/api'
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import AuthorizationResponse from '../store/authorization/api/response'
 import { ErrorResponse } from '../utility/type'
-import token from './token'
-
-type Response<Data> = {
-    data: Data
-    status: number
-}
+import tokenRepository from './tokenRepository'
 
 class Initialize {
     private static isRefreshed = false
 
     private static refresh = async () => {
         this.isRefreshed = true
-
-        const refreshResponse = await authorizationAPI.refresh()
-        if (!refreshResponse.data) {
-            token.delete()
-            return 
-        }
-
-        token.save(refreshResponse.data.accessToken)
+        return axios.get(process.env.NEXT_PUBLIC_BASE_URL + 'authorization/refresh', {withCredentials: true})
+            .then((response) => {
+                const refreshResponse: AuthorizationResponse.Refresh | ErrorResponse = response.data
+                return refreshResponse.data?.accessToken
+            })
     }
 
-    public static json = async <Data>(response: Response<Data | ErrorResponse>) => {
-        if (response.status < 400) {
-            this.isRefreshed = false
-            return response.data as Data
+    public static async requestOnFulfilled(config: AxiosRequestConfig) {
+        if (config.headers) {
+            config.headers.authorization = `Bearer ${tokenRepository.read() || ''}`
         }
 
-        if (this.isRefreshed || response.status !== 401) {
+        return config
+    }
+
+    public static async requestOnRejected(error: any) {
+        return error
+    }
+
+    public static async responseOnFulfilled(config: AxiosResponse) {
+        return config
+    }
+
+    public static async responseOnRejected(error: AxiosError) {
+        if (error.response?.status !== 401 || this.isRefreshed) {
             this.isRefreshed = false
-            return response.data as ErrorResponse
+            return 
         }
-
-        await this.refresh()
-
-        if (token.read() !== null) {
+        
+        const accessToken = await this.refresh()
+        typeof accessToken === 'string'
+            ? tokenRepository.save(accessToken)
+            : tokenRepository.delete()
+        
+        if (tokenRepository.read() !== null) {
             throw new Error() // Go to .catch() and re-request the original request 
         }
-
-        return response.data as ErrorResponse
     }
 }
 
