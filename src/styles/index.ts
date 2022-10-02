59cabe69a03fc2d841d555d25b547f0424a26b61
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-02 23:27:46 +0300

    Added styles

---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+const STYLES = {
+    'border-radius-50%': {
+        'border-radius': '50%'
+    },
+    'background-color-button-shadow': {
+        'background-color': 'ButtonShadow'
+    }
+}
+
+export default STYLES
