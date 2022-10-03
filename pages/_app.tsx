21440110b38610f1e82d556a9520b60fe1aaf457
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-03 13:27:58 +0300

    Added the global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
-import AtomProvider from '../src/packages/atom/Provider'
+import AtomProvider from '../src/packages/atom/provider'
 import STYLES from '../src/styles'
+
+import '../src/styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
