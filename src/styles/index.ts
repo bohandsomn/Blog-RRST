a3d904209fd278b09ac239ed424c1debed5ade95
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-03 13:26:26 +0300

    Added new styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,9 +1,30 @@
 const STYLES = {
-    'border-radius-50%': {
-        'border-radius': '50%'
+    "background-color--transparent-bg-color": {
+        "background-color": "var(--transparent-bg-color)"
     },
-    'background-color-button-shadow': {
-        'background-color': 'ButtonShadow'
+    "background-color--main-bg-color": {
+        "background-color": "var(--main-bg-color)"
+    },
+    "color--main-color": {
+        "color": "var(--main-color)"
+    },
+    "color--secondary-color": {
+        "color": "var(--secondary-color)"
+    },
+    "border-radius--border-radius": {
+        "border-radius": "var(--border-radius)"
+    },
+    "border-radius-50%": {
+        "border-radius": "50%"
+    },
+    "background-color-button-shadow": {
+        "background-color": "ButtonShadow"
+    },
+    "font-weight-bold": {
+        "font-weight": "bold"
+    },
+    "font-size-24": {
+        "font-size": "24px"
     }
 }
 
