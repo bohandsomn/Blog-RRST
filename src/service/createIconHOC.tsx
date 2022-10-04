43999c5f260ff03864de9af9876af771a8588890
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-04 14:45:19 +0300

    added create icon hoc for changing stroke

---
+++ src/service/createIconHOC.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import Toggle from '../context/toggle'
+import Icon from '../packages/icon'
+
+const createIconHOC = (MyIcon: ReturnType<typeof Icon.create>) => {
+    return React.forwardRef((props: NonNullable<typeof MyIcon.defaultProps>, ref) => {
+        const {value} = useContext(Toggle)
+
+        return (
+            <MyIcon 
+                fill="none" 
+                stroke={value ? 'var(--secondary-color)' : 'var(--main-color)'} 
+                {...props} 
+                ref={ref} 
+            />
+        )
+    })
+}
+
+export default createIconHOC
