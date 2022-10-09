8474e300ef47557b14aba5385896dce1758122f4
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-09 12:36:07 +0300

    Customizing style-combinator

---
+++ src/styles/app-create.ts
@@ -0,0 +1,18 @@
+import STYLES from '.'
+import { combinator } from '../packages/style-combinator'
+
+const create = combinator([
+    {screenClass: 'extraSmall', maxWidth: 575},
+    {screenClass: 'small', minWidth: 576},
+    {screenClass: 'medium', minWidth: 768},
+    {screenClass: 'large', minWidth: 992},
+    {screenClass: 'extraLarge', minWidth: 1200}
+])
+
+const appCreate = <ClassName extends string>(
+    ...args: Parameters<typeof create<ClassName, keyof typeof STYLES>>
+) => create(...args)
+
+export type ScreenClass = keyof Parameters<typeof create>['0']['styles'][string]
+
+export default appCreate
