bbd106b076132158f8919f2e7b7267c77a0ada26
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-09 13:25:39 +0300

    Add styles

--- src/service/component-explorer/AtomProviderStorybook.tsx
+++ src/service/component-explorer/AtomProviderStorybook.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import STYLES from '../../styles'
 import AtomProvider from '../../packages/atom/provider'
+import { StyleCombinatorProvider } from '../../packages/style-combinator'
+import createdStyles from '../../styles/created-styles'
 
 const AtomProviderStorybook: React.FC<Props> = ({ children }) => {
     return (
         <AtomProvider styles={STYLES}>
-            {children}
+            <StyleCombinatorProvider styles={createdStyles}>
+                {children}
+            </StyleCombinatorProvider>
         </AtomProvider>
     )
 }
