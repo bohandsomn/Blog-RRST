864f041ff10c2d066ad6415ab597ff437c6787b4
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-15 11:42:34 +0300

    Added white-space-normal and word-break-break-word style

--- src/components/atoms/P/P.tsx
+++ src/components/atoms/P/P.tsx
@@ -5,28 +5,28 @@ import Atom from '../../../packages/atom'
 export const P = Atom.create('p')
 
 export const MainParagraph = React.forwardRef(({className, ...props}: NonNullable<typeof P.defaultProps>, ref) => {
-    const localClassName = useAppStyles('color--main-color', className as undefined)
+    const localClassName = useAppStyles('color--main-color', 'white-space-normal', 'word-break-break-word', className as undefined)
     return (
         <P className={localClassName} {...props} ref={ref} />
     )
 })
 
 export const MainBoldParagraph = React.forwardRef(({className, ...props}: NonNullable<typeof P.defaultProps>, ref) => {
-    const localClassName = useAppStyles('color--main-color', 'font-weight-bold', className as undefined)
+    const localClassName = useAppStyles('color--main-color', 'font-weight-bold', 'white-space-normal', 'word-break-break-word', className as undefined)
     return (
         <P className={localClassName} {...props} ref={ref} />
     )
 })
 
 export const MainBigParagraph = React.forwardRef(({className, ...props}: NonNullable<typeof P.defaultProps>, ref) => {
-    const localClassName = useAppStyles('color--main-color', 'font-weight-bold', 'font-size-24', className as undefined)
+    const localClassName = useAppStyles('color--main-color', 'font-weight-bold', 'font-size-24', 'white-space-normal', 'word-break-break-word', className as undefined)
     return (
         <P className={localClassName} {...props} ref={ref} />
     )
 })
 
 export const SecondaryParagraph = React.forwardRef(({className, ...props}: NonNullable<typeof P.defaultProps>, ref) => {
-    const localClassName = useAppStyles('color--secondary-color', className as undefined)
+    const localClassName = useAppStyles('color--secondary-color', 'white-space-normal', 'word-break-break-word', className as undefined)
     return (
         <P className={localClassName} {...props} ref={ref} />
     )
