dbd5ef823e11d8744d60fb7e9cf0de88b071ad16
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-15 11:40:29 +0300

    Added default fill

--- src/service/icon/createIconHOC.tsx
+++ src/service/icon/createIconHOC.tsx
@@ -3,12 +3,12 @@ import Toggle from '../../context/toggle'
 import Icon from '../../packages/icon'
 
 const createIconHOC = (MyIcon: ReturnType<typeof Icon.create>) => {
-    return React.forwardRef((props: NonNullable<typeof MyIcon.defaultProps>, ref) => {
+    return React.forwardRef(({fill='none', ...props}: NonNullable<typeof MyIcon.defaultProps>, ref) => {
         const {value} = useContext(Toggle)
 
         return (
             <MyIcon 
-                fill="none" 
+                fill={fill} 
                 stroke={value ? 'var(--secondary-color)' : 'var(--main-color)'} 
                 {...props} 
                 ref={ref} 
