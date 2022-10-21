93730a8f54c61ede026ae1181012f70edd24d3ce
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-21 22:16:40 +0300

    create authorization store

---
+++ src/store/authorization/interface.ts
@@ -0,0 +1,5 @@
+import type UserResponse from '../user/api/response'
+import type AuthorizationResponse from './api/response'
+
+export type UserDTO = UserResponse.UserDTO
+export type UserData = AuthorizationResponse.UserData
