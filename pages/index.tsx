68030934e484fc0e95beb32272fb218d02bafbc1
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-23 15:25:36 +0300

    Fetched post list via getStaticProps & passed it to provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import React from 'react'
 import { AppHead } from '../src/layouts/head'
+import { PostListProvider, postNotifier } from '../src/feature/post'
 
-const Home: NextPage = () => {
+const Home: NextPage<Props> = ({ posts }) => {
     return (
         <AppHead title="Home">
-            <div>Home page</div>
+            <PostListProvider posts={posts}>
+                <div>Home page</div>
+            </PostListProvider>
         </AppHead>
     )
+}
+
+type Props = {
+    posts: Awaited<ReturnType<typeof postNotifier.getMany.call>>
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const posts = await postNotifier.getMany.call({
+        title: '',
+        content: '',
+        page: '1',
+        privacy: 'PUBLIC'
+    })
+
+    return {
+        props: {
+            posts
+        }
+    }
 }
 
 export default Home
