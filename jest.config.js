c6aa89686f241bb7aec1da6c7f86fea52b423fc2
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-27 15:09:46 +0300

    added aliases to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   moduleDirectories: ['node_modules', __dirname],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/packages/(.*)$': '<rootDir>/src/packages/$1',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+  },
 }
 
 module.exports = createJestConfig(customJestConfig);
