732627fb6dda5cf215decc906a4b8d0fa6dde59e
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-27 10:17:54 +0300

    Added styles

--- src/feature/post/components/post-list/PostList.tsx
+++ src/feature/post/components/post-list/PostList.tsx
@@ -4,11 +4,14 @@ import PostListContext from '../../context/post-list'
 import PostProvider from '../../provider/Post'
 import Post from './post'
 import UserProvider from '../../provider/User'
+import useAppQuery from '../../../../hooks/useAppQuery'
 
 const PostList: React.FC = () => {
     const postListContext = useContext(PostListContext)
+    const query = useAppQuery('post-list')
+    
     return (
-        <Ul>
+        <Ul query={query}>
             {
                 postListContext.data?.map((post) => (
                     <Li key={post.id}>
--- src/feature/post/components/post-list/post/Post.tsx
+++ src/feature/post/components/post-list/post/Post.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { SecondaryBackground, SecondaryContainer } from '../../../../../layouts/wrapper'
+import { SecondaryBackground } from '../../../../../layouts/wrapper'
 import UpdateToggleProvider from '../../../provider/UpdateToggle'
 import Comment from './comment'
 import PostData from './post-data'
 
 const Post: React.FC = () => {
     return (
-        <SecondaryContainer>
-            <SecondaryBackground>
-                <UpdateToggleProvider>
-                    <PostData />
-                </UpdateToggleProvider>
-                <Comment />
-            </SecondaryBackground>
-        </SecondaryContainer>
+        <SecondaryBackground>
+            <UpdateToggleProvider>
+                <PostData />
+            </UpdateToggleProvider>
+            <Comment />
+        </SecondaryBackground>
     )
 }
 
