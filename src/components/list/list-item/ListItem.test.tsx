a9c5f5738bd8dca3d59f0bdbfc29d5f2ccdf3d0f
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-27 15:09:09 +0300

    created test list molecules

--- src/components/list/list-item/ListItem.test.tsx
+++ src/components/list/list-item/ListItem.test.tsx
@@ -1,23 +1,39 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { AtomProvider } from '@/packages/atom'
+import { StyleCombinatorProvider } from '@/packages/style-combinator'
 import { ListItem } from './ListItem'
 import { Account } from '../../atoms'
 import ToggleProvider from '../../../provider/toggle'
+import STYLES from 'src/styles'
+import createdStyles from 'src/styles/created-styles'
 
 describe('ListItem', () => {
     test('should renders the element', () => {
-        render(<ToggleProvider><ListItem>ListItem</ListItem></ToggleProvider>)
+        render(
+            <AtomProvider styles={STYLES}>
+                <StyleCombinatorProvider styles={createdStyles}>
+                    <ToggleProvider>
+                        <ListItem>ListItem</ListItem>
+                    </ToggleProvider>
+                </StyleCombinatorProvider>
+            </AtomProvider>
+        )
         const element = screen.getByText(/ListItem/)
         expect(element).toBeInTheDocument()
     })
 
     test('should containg svg', () => {
         render(
-            <ToggleProvider>
-                <ListItem right={<Account data-testid="svg" />}>
-                    ListItem
-                </ListItem>
-            </ToggleProvider>
+            <AtomProvider styles={STYLES}>
+                <StyleCombinatorProvider styles={createdStyles}>
+                    <ToggleProvider>
+                        <ListItem right={<Account data-testid="svg" />}>
+                            ListItem
+                        </ListItem>
+                    </ToggleProvider>
+                </StyleCombinatorProvider>
+            </AtomProvider>
         )
         const element = screen.getByTestId('svg')
         expect(element).toBeInTheDocument()
@@ -25,11 +41,15 @@ describe('ListItem', () => {
     
     test('should changes classList', () => {
         render(
-            <ToggleProvider>
-                <ListItem right={<Account />}>
-                    ListItem
-                </ListItem>
-            </ToggleProvider>
+            <AtomProvider styles={STYLES}>
+                <StyleCombinatorProvider styles={createdStyles}>
+                    <ToggleProvider>
+                        <ListItem right={<Account />}>
+                            ListItem
+                        </ListItem>
+                    </ToggleProvider>
+                </StyleCombinatorProvider>
+            </AtomProvider>
         )
         const initialElement = screen.getByText(/ListItem/)
         fireEvent.mouseEnter(initialElement)
