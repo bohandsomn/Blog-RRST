846cba506f28c9978c9cb547d559641e75a15c51
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-27 15:08:26 +0300

    created test for hooks

--- src/packages/style-combinator/hooks/useCombinedStyles/index.ts
+++ src/packages/style-combinator/hooks/useCombinedStyles/index.ts
@@ -1,5 +1,5 @@
 import { useContext, useMemo } from 'react'
-import StyleCombinator from '../context/style-combinator'
+import StyleCombinator from '../../context/style-combinator'
 
 const useCombinedStyles = <StyleKey extends string>(className: StyleKey) => {
     const {value} = useContext(StyleCombinator)
