1254cdfa607a926a845ee021331659bec0eba589
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-28 10:25:15 +0300

    created test for hooks

--- src/hooks/useAppQuery/index.ts
+++ src/hooks/useAppQuery/index.ts
@@ -2,9 +2,9 @@ import { useContext, useMemo } from 'react'
 import { classNameAdapter, AtomContext, Query } from '@/packages/atom'
 import { useCombinedStyles, Context, ICreatedStyles, getMediaQuery } from '@/packages/style-combinator'
 
-import STYLES from '../styles'
-import type { ScreenClass } from '../styles/app-create'
-import type { AppClassName } from '../styles/created-styles'
+import STYLES from '../../styles'
+import type { ScreenClass } from '../../styles/app-create'
+import type { AppClassName } from '../../styles/created-styles'
 
 const useAppQuery = <ClassName extends AppClassName>(
     className: ClassName, 
@@ -12,9 +12,8 @@ const useAppQuery = <ClassName extends AppClassName>(
 ) => {
     const combinedStyles = useCombinedStyles(className) as ICreatedStyles<ClassName, ScreenClass, keyof typeof STYLES>[ClassName]
     const atomContext = useContext(AtomContext)
-    const styleClasses = Object.keys(combinedStyles) as ScreenClass[]
-
     return useMemo(() => {
+        const styleClasses = Object.keys(combinedStyles) as ScreenClass[]
         return styleClasses.reduce((accumulator, styleClass) => {
             const createdStyle = combinedStyles[styleClass]
             const mediaQuery = getMediaQuery(createdStyle)
@@ -27,7 +26,7 @@ const useAppQuery = <ClassName extends AppClassName>(
                 [mediaQuery]: styles
             }
         }, {} as Query)
-    }, [combinedStyles, atomContext, styleClasses, context])
+    }, [combinedStyles, atomContext, context])
 }
 
 export default useAppQuery
