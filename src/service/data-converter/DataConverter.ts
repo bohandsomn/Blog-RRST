4479e8f727972a876bc15fbefdb384efb205182d
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-29 23:32:01 +0300

    created data-converter service

---
+++ src/service/data-converter/DataConverter.ts
@@ -0,0 +1,29 @@
+class DataConverter {
+    private static toPascalCase(month: string) {
+        const block = "\\w\\u0400-\\u04FF"
+        const regExp = new RegExp("([^" + block + "]|^)([" + block + "])", "g")
+        return month.replace(regExp, (_, first, second) => { 
+            return first + second.toUpperCase()
+        })
+    }
+
+    static getISO(date: Date) {
+        return date.toISOString()
+    }
+
+    static getDateMonthNameDataYear(date: Date) {
+        const month = date.toLocaleString('default', { month: 'long' })
+        const dataOfMonth = date.getDate()
+        const year = date.getFullYear()
+        return `${this.toPascalCase(month)}, ${dataOfMonth}/${year}`
+    }
+
+    static getMonthDateYear(date: Date) {
+        const dataOfMonth = date.getDate()
+        const month = date.getMonth()
+        const year = date.getFullYear()
+        return `${month + 1}/${dataOfMonth}/${year}`
+    }
+}
+
+export default DataConverter
