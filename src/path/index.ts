7298e22c3df57e34ba896ca61d319092491988fa
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-29 18:49:44 +0300

    Added users path

--- src/path/index.ts
+++ src/path/index.ts
@@ -2,7 +2,8 @@ enum PATH {
     HOME = '/',
     REGISTRATION = '/registration',
     LOG_IN = '/log-in',
-    SEARCH_POSTS='/search-posts'
+    SEARCH_POSTS='/search-posts',
+    USERS='/users'
 }
 
 export default PATH
