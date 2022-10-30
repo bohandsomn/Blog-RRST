90f92938b78494ffa5817b3955394e5870b116b5
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-30 18:30:39 +0200

    created friendships notifier

--- src/store/friendships/notifier/index.ts
+++ src/store/friendships/notifier/index.ts
@@ -5,19 +5,11 @@ import friendshipsAPI from '../api'
 const notification = new Notification()
 
 const subscribers = new RequestNotifier<typeof friendshipsAPI.subscribers>({
-    apiMethod: (...data) => friendshipsAPI.subscribers(...data),
-    before: () => notification.loading('Getting subscribers started'),
-    narrowing: (response) => response.data !== undefined,
-    success: ({ id, response }) => notification.success(id, response.message),
-    reject: ({ id, response }) => notification.error(id, response.message)
+    apiMethod: (...data) => friendshipsAPI.subscribers(...data)
 })
 
 const subscriptions = new RequestNotifier<typeof friendshipsAPI.subscriptions>({
-    apiMethod: (...data) => friendshipsAPI.subscriptions(...data),
-    before: () => notification.loading('Getting subscriptions started'),
-    narrowing: (response) => response.data !== undefined,
-    success: ({ id, response }) => notification.success(id, response.message),
-    reject: ({ id, response }) => notification.error(id, response.message)
+    apiMethod: (...data) => friendshipsAPI.subscriptions(...data)
 })
 
 const friendshipsNotifier = {
