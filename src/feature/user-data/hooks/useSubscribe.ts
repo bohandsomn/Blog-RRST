6f533066af80155522a7351e93d09d3e94ce5975
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-30 13:44:23 +0200

    changed subscribe provider

--- src/feature/user-data/hooks/useSubscribe.ts
+++ src/feature/user-data/hooks/useSubscribe.ts
@@ -1,18 +1,28 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useContext, useEffect, useMemo } from 'react'
 import { subscribeNotifier } from '../api'
-import useFetch, { State } from '../../../hooks/useFetch'
-import type { ErrorResponse } from '../../../utility/type'
+import useFetch from '../../../hooks/useFetch'
+import UserDataContext from '../context/user-data'
 import type { ISubscribe } from '../context/subscribe'
 import type { SubscribeResponse } from '../interface'
 
-const useSubscribe = (serverSubscribe: ErrorResponse | SubscribeResponse.GetSubscribe): ISubscribe => {
-    const initialSubscribe: State<SubscribeResponse.SubscribeDTO> = useMemo(() => ({
-        data: serverSubscribe.data || null,
-        isLoading: false,
-        error: serverSubscribe.data === undefined ? serverSubscribe.message : null
-    }), [serverSubscribe])
-    const { data, previous, isLoading, error, onSuccess, onReject, onPending } = useFetch<SubscribeResponse.SubscribeDTO | null>(initialSubscribe)
-
+const useSubscribe = (): ISubscribe => {
+    const user = useContext(UserDataContext)
+    const { data, previous, isLoading, error, onSuccess, onReject, onPending } = useFetch<SubscribeResponse.SubscribeDTO | null>()
+    useEffect(() => {
+        const handleGetSubscribe = async () => {
+            onPending()
+            const subscribeResponse = await subscribeNotifier.subscribe.call({
+                subscriberId: user.id.toString()
+            })
+            if (subscribeResponse.data !== undefined) {
+                onSuccess(subscribeResponse.data)
+            } else {
+                onReject(subscribeResponse.message)
+            }
+        }
+        handleGetSubscribe()
+    }, [])
+    
     const subscribe: ISubscribe['subscribe'] = useCallback(async (dataSubscribe) => {
         onPending()
         const subscribeResponse = await subscribeNotifier.subscribe.call(dataSubscribe)
--- src/feature/user-data/provider/Subscribe.tsx
+++ src/feature/user-data/provider/Subscribe.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import SubscribeContext from '../context/subscribe'
 import useSubscribe from '../hooks/useSubscribe'
-import type { ErrorResponse } from '../../../utility/type'
-import type { SubscribeResponse } from '../interface'
 
-const SubscribeProvider: React.FC<Props> = ({ children, subscribe }) => {
-    const value = useSubscribe(subscribe)
+const SubscribeProvider: React.FC<Props> = ({ children }) => {
+    const value = useSubscribe()
     return (
         <SubscribeContext.Provider value={value}>
             {children}
@@ -15,7 +13,6 @@ const SubscribeProvider: React.FC<Props> = ({ children, subscribe }) => {
 
 type Props = {
     children?: any
-    subscribe: ErrorResponse | SubscribeResponse.GetSubscribe
 }
 
 export default SubscribeProvider
