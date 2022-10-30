875bd206d3202d2c5794e669de6d2103ee668195
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-30 13:04:27 +0200

    reimported subscribe api

--- src/feature/user-data/api/index.ts
+++ src/feature/user-data/api/index.ts
@@ -1,7 +1,9 @@
 import { userNotifier } from '../../../store/user'
+import { subscribeNotifier } from '../../../store/subscribe'
 import * as authorizationSlices from '../../../store/authorization'
 
 export {
     userNotifier,
-    authorizationSlices
+    authorizationSlices,
+    subscribeNotifier
 }
